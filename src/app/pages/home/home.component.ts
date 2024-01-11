12e605670ee78aa5c43720fb9a54b14c0e536d69
Sarvar ibn Murod <123171146+murodovich@users.noreply.github.com>
2024-01-11 21:47:28 +0500

    eqw

---
+++ src/app/pages/home/home.component.ts
@@ -0,0 +1,108 @@
+import {AfterViewInit, Component, ViewChild} from '@angular/core';
+import {MatPaginator, MatPaginatorModule} from '@angular/material/paginator';
+import {MatSort, MatSortModule} from '@angular/material/sort';
+import {MatTableDataSource, MatTableModule} from '@angular/material/table';
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
+
+export interface UserData {
+  id: string;
+  productName: string;
+  category: string;
+  condition: string;
+  price: number;
+  comment: string;
+  date: string;
+}
+
+/** Constants used to fill up our data base. */
+const FRUITS: string[] = [
+  'blueberry',
+  'lychee',
+  'kiwi',
+  'mango',
+  'peach',
+  'lime',
+  'pomegranate',
+  'pineapple',
+];
+const NAMES: string[] = [
+  'Maia',
+  'Asher',
+  'Olivia',
+  'Atticus',
+  'Amelia',
+  'Jack',
+  'Charlotte',
+  'Theodore',
+  'Isla',
+  'Oliver',
+  'Isabella',
+  'Jasper',
+  'Cora',
+  'Levi',
+  'Violet',
+  'Arthur',
+  'Mia',
+  'Thomas',
+  'Elizabeth',
+];
+
+/**
+ * @title Data table with sorting, pagination, and filtering.
+ */
+@Component({
+  selector: 'app-home',
+  styleUrls: ['./home.component.html'],
+  templateUrl: './home.component.html',
+  standalone: true,
+  imports: [MatFormFieldModule, MatInputModule, MatTableModule, MatSortModule, MatPaginatorModule],
+})
+export class HomeComponent implements AfterViewInit {
+  displayedColumns: string[] = ['id', 'productname', 'category', 'condition','price', 'comment', 'date'];
+  dataSource: MatTableDataSource<UserData>;
+
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+  @ViewChild(MatSort) sort!: MatSort;
+
+  constructor() {
+    // Create 100 users
+    const users = Array.from({length: 100}, (_, k) => createNewUser(k + 1));
+
+    // Assign the data to the data source for the table to render
+    this.dataSource = new MatTableDataSource(users);
+  }
+
+  ngAfterViewInit() {
+    this.dataSource.paginator = this.paginator;
+    this.dataSource.sort = this.sort;
+  }
+
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+
+    if (this.dataSource.paginator) {
+      this.dataSource.paginator.firstPage();
+    }
+  }
+}
+
+/** Builds and returns a new User. */
+function createNewUser(id: number): UserData {
+  const name =
+    NAMES[Math.round(Math.random() * (NAMES.length - 1))] +
+    ' ' +
+    NAMES[Math.round(Math.random() * (NAMES.length - 1))].charAt(0) +
+    '.';
+
+  return {
+    id: id.toString(),
+    productName: name,
+    category: Math.round(Math.random() * 100).toString(),
+    condition: FRUITS[Math.round(Math.random() * (FRUITS.length - 1))],
+    price: Math.round(Math.random() * (FRUITS.length - 1)),
+    comment: FRUITS[Math.round(Math.random() * (FRUITS.length - 1))],
+    date: FRUITS[Math.round(Math.random() * (FRUITS.length - 1))],
+  };
+}
